d02ace67cf1078cbaceb57ec46384d9af1cf5b9f
Maria <alwaysmaria1@gmail.com>
2021-05-06 21:14:57 -0700

    Complete Iteration 2

--- maria_emojinal/ViewController.swift
+++ maria_emojinal/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let emojis = ["🥸":"fool","😢":"crying"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -16,7 +18,8 @@ class ViewController: UIViewController {
     @IBAction func showMessage(sender: UIButton) {
         //when button is clicked
         //alert will pop up with a generated message
-        let alertController = UIAlertController(title: "Why?", message: "I can't do math", preferredStyle: UIAlertController.Style.alert)
+        let selectedEmotion = sender.titleLabel?.text
+        let alertController = UIAlertController(title: "\(titletext)", message: "\(messageText)", preferredStyle: UIAlertController.Style.alert)
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         present(alertController, animated: true, completion: nil)
     }
